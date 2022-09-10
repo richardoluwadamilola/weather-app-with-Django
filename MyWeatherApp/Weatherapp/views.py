5daaf937d125089f83b057d9f64a2424ce1cffdd
richardoluwadamilola <oluwatobyrichard@gmail.com>
2022-09-10 20:05:13 +0100

    pratice weather app

---
+++ MyWeatherApp/Weatherapp/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+import requests
+from .models import City
+from .forms import CityForm
+# Create your views here.
+def index(request):
+    url= 'http://api.openweathermap.org/data/2.5/weather?q=las%20vegas&units=imperial&appid=272c1fb240a64bbc5237e4f1d0fc2351'
+
+    cities = City.objects.all() #return all the cities in the database
+
+    if request.method == 'POST': # only true if form is submitted
+        form = CityForm(request.POST) # add actual request data to form for processing
+        form.save() # will validate and save if validate
+
+    form = CityForm()
+
+    weather_data = []
+    
+    for city in cities:
+
+        city_weather = requests.get(url.format(city)).json()
+
+
+    weather = {
+        'city' : city,
+        'temperature' : city_weather['main']['temp'],
+        'description' : city_weather['weather'][0]['description'],
+        'icon' : city_weather['weather'][0]['icon']
+    }
+
+    weather_data.append(weather) #add the data for the current city into our list
+    
+    context = {'weather_data' : weather_data, 'form' : form}
+
+    return render(request, 'index.html', context)
